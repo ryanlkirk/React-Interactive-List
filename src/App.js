8ae70ec3837e7086be610ba89fc40fa92dfa92e9
Ryan Kirk <ryan.kirk@junipereducation.org>
2021-08-12 17:00:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Table from "./Components/Table.js";
+
+function App() {
+    return (
+        <div className="App">
+            <Table></Table>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Table.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Table = (props) => {
+    return <div>Hello World</div>;
+};
+
+export default Table;
